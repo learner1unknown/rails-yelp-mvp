2a53d5c0f714a33f6ca253d35081ae1a39aed161
reuben frith <reubenfrith007@gmail.com>
2022-11-12 11:44:30 +1100

    made models and controllers for review and restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants do
+    resources :reviews, only: [:new]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating data samples"
+
+20.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  phone_number = Faker::Number.number(digits: 10)
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create(name: name, address: address, phone_number: phone_number, category: category)
+end
+
+puts "Finished creatigng 20 restaurant samples"
